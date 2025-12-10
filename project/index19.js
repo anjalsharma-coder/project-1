4cfce2b011121df93a9f2f02b8c4df340b3a84d7
anjalsharma-coder <anjal.sharma3228@gmail.com>
2025-12-11 12:11:49 +1300

    simple calculator project

---
+++ project/index19.js
@@ -0,0 +1,25 @@
+//Addition
+var x = 0;
+var p = 10;
+var a = x + p;
+console.log(a);
+//Subtraction 
+let q = 876585;
+let w = 10;
+let e = p - q;
+console.log(e);
+//Multiplication
+const u = 98;
+const y = 77;
+const o = u*y;
+console.log(o);
+//Divison
+var i = 88;
+var s = 11;
+var x = i/s;
+console.log(x);
+//module operator
+let g = 19;
+let j = 5;
+let b = g%j;
+console.log(b);
